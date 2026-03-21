fa148471b1d4a0339e3a2761f37b60cf85d10eae
Khushi-it <khushimasram2006@gmail.com>
2026-03-22 00:42:05 +0530

    Update first.java

--- javapractical10/first.java
+++ javapractical10/first.java
@@ -1,18 +1,19 @@
-class Animal{
-          void sound(){
-     System.out.println("Different Sounds of Animals:");
-   }
- }
+class Animal {
+    void sound() {
+        System.out.println("Different Sounds of Animals:");
+    }
+}
 
-class Cat extends Animal{
-     void meow(){
-   System.out.println("this is sound of cat");
-  }
-public class First{
-   public static  void main(String[]  args){
-      Cat c =new Cat();
-      c.sound();
-      c.meow();
-  }
- }
-}    
+class Cat extends Animal {
+    void meow() {
+        System.out.println("This is sound of cat");
+    }
+}
+
+public class First {
+    public static void main(String[] args) {
+        Cat c = new Cat();
+        c.sound();
+        c.meow();
+    }
+}  
